f9d531b66c92412a23d7801368350f3b56c6a3ad
Antony Veliz <antonyvelizgonzales@gmail.com>
2026-03-08 19:05:33 -0500

    Segundo commit

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function verificarPago() {
+  let nombre = document.getElementById("nombre").value;
+  let fecha = document.getElementById("fecha").value;
+  let hobbies = document.getElementById("hobbies").value;
+  let pago = document.getElementById("pago").value;
+
+  let mensaje = document.getElementById("mensaje");
+
+  if (nombre === "" || fecha === "" || hobbies === "") {
+    mensaje.innerText = "Complete todos los campos";
+    return;
+  }
+
+  if (pago === "no") {
+    mensaje.innerText = "Ud aún no ha completado su pago";
+    mensaje.style.color = "red";
+  } else {
+    mensaje.innerText = "Completado su registro";
+    mensaje.style.color = "green";
+  }
+}
